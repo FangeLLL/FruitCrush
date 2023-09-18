de69e1408d8b04dc128b37b437914229ef5458ea
Bertuğ Abalı <bertugabali@hotmail.com>
2023-09-18 17:53:30 +0300

    Improved Grid Algo.
    Multiple fruits pop now available.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Jobs;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Board : MonoBehaviour
@@ -16,17 +18,19 @@ public class Board : MonoBehaviour
     public GameObject[] fruits;
 
     public GameObject[,] allFruits;
+    public GameObject[,] allTiles;
 
     void Start()
     {
         allFruits = new GameObject[width, height];
+        allTiles = new GameObject[width, height];
         SetUp();
     }
 
     private void SetUp()
     {
-         xOffset = width * 0.5f - 0.5f;
-         yOffset = height * 0.5f - 0.5f;
+        xOffset = width * 0.5f - 0.5f;
+        yOffset = height * 0.5f - 0.5f;
 
         for (int i = 0; i < width; i++)
         {
@@ -36,6 +40,7 @@ public class Board : MonoBehaviour
                 GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
                 backgroundTile.transform.parent = this.transform;
                 backgroundTile.name = "( " + i + ", " + j + " )";
+                allTiles[i, j] = backgroundTile;
 
                 int fruitToUse = Random.Range(0, fruits.Length);
                 GameObject fruit = Instantiate(fruits[fruitToUse], tempPosition, Quaternion.identity);
@@ -48,21 +53,21 @@ public class Board : MonoBehaviour
             }
         }
     }
-   
-    
 
-    public void MoveFruits(float swipeAngle,int column,int row)
+
+
+    public void MoveFruits(float swipeAngle, int column, int row)
     {
         GameObject otherFruit;
         GameObject fruit = allFruits[column, row];
         Fruit fruitScript = fruit.GetComponent<Fruit>();
-        Vector2 currentFruitLoc= fruit.transform.position;
+        Vector2 currentFruitLoc = fruit.transform.position;
         Vector2 otherFruitLoc;
         if (swipeAngle > -45 && swipeAngle <= 45 && column < width)
         {
             // RIGHT SWIPE
             otherFruit = allFruits[column + 1, row];
-            otherFruitLoc= otherFruit.transform.position;
+            otherFruitLoc = otherFruit.transform.position;
             otherFruit.GetComponent<Fruit>().column -= 1;
             otherFruit.GetComponent<Fruit>().targetV = currentFruitLoc;
             fruitScript.column++;
@@ -100,7 +105,6 @@ public class Board : MonoBehaviour
         }
         StartCoroutine(CheckAndDestroyMatches());
     }
-
     public IEnumerator CheckAndDestroyMatches()
     {
         yield return null;
@@ -110,40 +114,100 @@ public class Board : MonoBehaviour
         {
             for (int j = 0; j < height - 2; j++)
             {
-                GameObject fruit1 = allFruits[i, j];
-                GameObject fruit2 = allFruits[i, j + 1];
-                GameObject fruit3 = allFruits[i, j + 2];
-
-                // Check if the fruits have the same type (you can implement this check in your Fruit script)
-                if (fruit1.GetComponent<Fruit>().fruitType == fruit2.GetComponent<Fruit>().fruitType &&
-                    fruit1.GetComponent<Fruit>().fruitType == fruit3.GetComponent<Fruit>().fruitType)
+                GameObject[] fruitsCheck;
+                fruitsCheck = new GameObject[height];
+                bool same = true;
+                int k = 0;
+                while (same)
                 {
-                    allFruits[i, j].GetComponent<Renderer>().enabled = false;
-                    allFruits[i, j + 1].GetComponent<Renderer>().enabled = false;
-                    allFruits[i, j + 2].GetComponent<Renderer>().enabled = false;
+                    if (j + k + 1 < height && allFruits[i, j + k].GetComponent<Fruit>().fruitType == allFruits[i, j + k + 1].GetComponent<Fruit>().fruitType)
+                    {
+                        fruitsCheck[k] = allFruits[i, j + k];
+                        k++;
+                    }
+                    else
+                    {
+                        fruitsCheck[k] = allFruits[i, j + k];
+                        k++;
+                        same = false;
+                    }
                 }
+
+                if (k >= 3)
+                {
+                    for (int e = 0; e < k; e++)
+                    {
+                        StartCoroutine(FadeOut(fruitsCheck[e]));
+                    }
+
+                    j += k;
+                }
+
+
             }
         }
+
 
         // Check for matches in rows (similar logic as columns)
         for (int j = 0; j < height; j++)
         {
             for (int i = 0; i < width - 2; i++)
             {
-                GameObject fruit1 = allFruits[i, j];
-                GameObject fruit2 = allFruits[i + 1, j];
-                GameObject fruit3 = allFruits[i + 2, j];
-
-                // Check if the fruits have the same type
-                if (fruit1.GetComponent<Fruit>().fruitType == fruit2.GetComponent<Fruit>().fruitType &&
-                    fruit1.GetComponent<Fruit>().fruitType == fruit3.GetComponent<Fruit>().fruitType)
+                GameObject[] fruitsCheck;
+                fruitsCheck = new GameObject[width];
+                bool same = true;
+                int k = 0;
+                while (same)
                 {
-                    allFruits[i, j].GetComponent<Renderer>().enabled = false;
-                    allFruits[i, j + 1].GetComponent<Renderer>().enabled = false;
-                    allFruits[i, j + 2].GetComponent<Renderer>().enabled = false;
+                    if (i + k + 1 < width && allFruits[i + k, j].GetComponent<Fruit>().fruitType == allFruits[i + k + 1, j].GetComponent<Fruit>().fruitType)
+                    {
+                        fruitsCheck[k] = allFruits[i + k, j];
+                        k++;
+                    }
+                    else
+                    {
+                        fruitsCheck[k] = allFruits[i + k, j];
+                        k++;
+                        same = false;
+                    }
+                }
+
+                if (k >= 3)
+                {
+                    for (int e = 0; e < k; e++)
+                    {
+                        StartCoroutine(FadeOut(fruitsCheck[e]));
+                    }
+
+                    i += k;
                 }
             }
         }
+
+    }
+
+    private IEnumerator FadeOut(GameObject fruit)
+    {
+        float elapsedTime = 0f;
+        float fadeDuration = 0.5f;
+        Color color = fruit.GetComponent<SpriteRenderer>().color;
+
+        while (elapsedTime < fadeDuration)
+        {
+            // Calculate the new alpha value
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+
+            // Update the object's color with the new alpha
+            fruit.GetComponent<SpriteRenderer>().color = new Color(color.r, color.g, color.b, alpha);
+
+            elapsedTime += Time.deltaTime;
+            yield return null; // Wait for the next frame
+        }
+
+        // Ensure the object is completely transparent
+
+        fruit.GetComponent<SpriteRenderer>().color = new Color(color.r, color.g, color.b, 0f);
+        ReplaceDestroyedFruit(fruit.GetComponent<Fruit>().column, fruit.GetComponent<Fruit>().row);
 
     }
 
@@ -163,8 +227,11 @@ public class Board : MonoBehaviour
         // Set the column and row of the new fruit
         fruit.GetComponent<Fruit>().column = column;
         fruit.GetComponent<Fruit>().row = row;
+        fruit.GetComponent<Fruit>().fruitType = fruitToUse;
+
 
         // Add the new fruit to the allFruits array
+        Destroy(allFruits[column, row]);
         allFruits[column, row] = fruit;
     }
 }

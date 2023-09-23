0b48c1af6cf3613edb679c3c1a30b1b3f395db62
Malii47 <mhmetaydn1468@icloud.com>
2023-09-23 16:39:31 +0300

    Move Count, Task Tracking Fix

--- Assets/Scripts/TaskController.cs
+++ Assets/Scripts/TaskController.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TaskController : MonoBehaviour
 {
     //StrawBale Index = 0
     public int[] taskNumber = { 0, 0, 0, 0, 0};
-
+    public int moveCount;
+    public TextMeshProUGUI moveText;
     bool isLevelCompleted = false;
+
+    public void SetMoveCount(int _moveCount)
+    {
+        moveCount = _moveCount;
+        moveText.text = moveCount.ToString();
+    }
+
+    public void MovePlayed()
+    {
+        moveCount--;
+        moveText.text = moveCount.ToString();
+        
+        if (moveCount <= 0)
+        {
+            Debug.Log("Out of Moves");
+            LevelEndCheck();
+        }
+    }
 
     public void SetTask(int taskTypeIndex, int _taskNumber)
     {
@@ -22,16 +42,16 @@ public class TaskController : MonoBehaviour
 
     void LevelEndCheck()
     {
-        foreach (int element in taskNumber)
+        for (int i = 0; i < taskNumber.Length; i++)
         {
-            if (element != 0)
+            if (taskNumber[i] != 0)
             {
                 isLevelCompleted = false;
                 break;
             }
         }
 
-        if (isLevelCompleted)
+        if (isLevelCompleted && moveCount >= 0)
         {
             Debug.Log("Level Completed");
         }

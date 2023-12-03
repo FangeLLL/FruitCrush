28c059691f201d6b546c04b3eed48df636334c6b
Berkay Soydemir <berkaysoydemirr@gmail.com>
2023-12-03 17:31:07 +0300

    Rearranged Sound Scripts

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,58 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : Sounds
 {
-    public AudioClip[] sounds;
-    public AudioSource source;
-
-    private bool isAudioPaused = false;
-
-    private Vector2 previousWindowSize;
-
-    private void Awake()
-    {
-        source = GetComponent<AudioSource>();
-    }
-
-    private void Update()
-    {
-        if (HasWindowResized())
-        {
-            PauseAudioSources();
-        }
-        else
-        {
-            if (!isAudioPaused)
-            {
-                PlaySounds();
-            }
-        }
-
-        RecordWindowSize();
-    }
-
-    private bool HasWindowResized()
-    {
-        Vector2 currentWindowSize = new Vector2(Screen.width, Screen.height);
-        return currentWindowSize != previousWindowSize;
-    }
-
-    private void RecordWindowSize()
-    {
-        previousWindowSize = new Vector2(Screen.width, Screen.height);
-    }
-
-    private void PlaySounds()
-    {
-        source.UnPause();
-    }
-
-    private void PauseAudioSources()
-    {
-        source.Pause();
-    }
-
     public void Swipe()
     {
         source.clip = sounds[0];
@@ -176,6 +126,7 @@ public class AudioManager : MonoBehaviour
 
     public void Harvester()
     {
+        source.volume = 0.25f;
         source.clip = sounds[25];
         source.PlayOneShot(source.clip);
     }

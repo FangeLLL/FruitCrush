00856ca3e5f55d13a742e14a5bef00d4bb052b47
Berkay Soydemir <berkaysoydemirr@gmail.com>
2024-01-31 19:01:41 +0300

    Integrated new music

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MusicManager : Sounds
 {
+
     public static MusicManager Instance { get; private set; }
 
     private void Awake()
@@ -31,14 +32,48 @@ public class MusicManager : Sounds
     {
         if (SceneManager.GetActiveScene().name != "MainMenu")
         {
-            source.volume = 0;
+            source.Stop();
+            InGameMusicOne();
         }
     }
 
-    public void MainMenuMusic()
+    private void MainMenuMusic()
     {
         source.loop = true;
         source.clip = sounds[0];
         source.PlayOneShot(source.clip);
     }
+
+    private void InGameMusicOne()
+    {
+        source.clip = sounds[1];
+        source.PlayOneShot(source.clip);
+    }
+
+    /*private void FadeOutMusic()
+    {
+        if (source.volume > 0f && isExitingMainMenu)
+        {
+            source.volume -= Time.deltaTime / musicFadeDuration;
+        }
+        else
+        {
+            source.Stop();
+            isExitingMainMenu = false;
+            oneTime = false;
+        }
+    }
+
+    private void FadeInMusic()
+    {
+        InGameMusicOne();
+        if (source.volume <= 1f && isExitingMainMenu)
+        {                      
+            source.volume += Time.deltaTime / musicFadeDuration;
+        }
+        else
+        {
+            isEnteredLevels = false;
+        }
+    }*/
 }

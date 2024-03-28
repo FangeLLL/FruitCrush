30fedb582967d2996482c320ead2475a00fedc00
Berkay Soydemir <berkaysoydemirr@gmail.com>
2024-03-28 16:21:34 +0300

    Added TNT Merge shadows

--- Assets/Scripts/PowerUpCreationAnimationController.cs
+++ Assets/Scripts/PowerUpCreationAnimationController.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PowerUpCreationAnimationController : MonoBehaviour
 {
     private int isPowerUpCreated = Animator.StringToHash("isPowerUpCreated");
+    [SerializeField] private GameObject TNTShadows;
     public void PowerUpCreationStop()
     {
         GetComponent<Animator>().SetBool(isPowerUpCreated, false);
     } 
 
+    public void TNTShadowController()
+    {
+        TNTShadows.SetActive(true);
+    }
 }
